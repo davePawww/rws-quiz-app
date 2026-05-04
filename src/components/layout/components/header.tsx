3a438925d178cadb3e0211f4c08233fdc3928bb2
DavePaurillo <paurillo.dave@gmail.com>
2026-05-05 03:45:37 +0800

    chore: changed the titles

--- src/components/layout/components/header.tsx
+++ src/components/layout/components/header.tsx
@@ -17,14 +17,14 @@ export default function Header() {
       transition={{ duration: 0.8, ease: 'easeIn' }}
       className="flex flex-col items-center justify-between gap-1.5 md:flex-row md:gap-0"
     >
-      <h1 className="font-semibold">00-project-title</h1>
+      <h1 className="font-semibold">05-quiz-app</h1>
       <div className="flex items-center gap-1">
         <Button variant={'secondary'} onClick={toggleTheme} aria-label="Toggle theme">
           {theme === 'light' ? <SunIcon /> : <MoonIcon />}
         </Button>
         <Separator orientation="vertical" />
         <Button variant={'outline'} asChild>
-          <a href="https://www.x.com" target="_blank" rel="noopener noreferrer">
+          <a href="https://rws-quiz-app.vercel.app/" target="_blank" rel="noopener noreferrer">
             <FaGithub /> Code
           </a>
         </Button>
