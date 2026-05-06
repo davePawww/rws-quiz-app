d1eeba07165811cd55e75022d8bf1fba04608ca4
DavePaurillo <paurillo.dave@gmail.com>
2026-05-07 01:15:19 +0800

    refactor: navigate to results: - when time runs out at the last question - when the answer is submitted at the last question

--- src/features/quiz/question-page.tsx
+++ src/features/quiz/question-page.tsx
@@ -25,12 +25,20 @@ export default function QuestionPage() {
     if (selectedAnswer === questions[currentIndex].answer) {
       addTenPoints();
     }
-    void animateAndNavigate(
-      { opacity: 0, x: -100 },
-      { type: 'tween', duration: 0.6, ease: 'easeInOut' },
-      `/questions/${questions[currentIndex + 1]?.id}`,
-      incrementCurrentIndex,
-    );
+    if (currentIndex + 1 === questions.length) {
+      return void animateAndNavigate(
+        { opacity: 0, x: -100 },
+        { type: 'tween', duration: 0.6, ease: 'easeInOut' },
+        `/results`,
+      );
+    } else {
+      void animateAndNavigate(
+        { opacity: 0, x: -100 },
+        { type: 'tween', duration: 0.6, ease: 'easeInOut' },
+        `/questions/${questions[currentIndex + 1]?.id}`,
+        incrementCurrentIndex,
+      );
+    }
   };
 
   return (
